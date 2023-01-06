b2304991d3591b68aad6b8d64d450afcd4690d04
David Hernandez <desdeelfrionorte@gmail.com>
2023-01-06 14:40:38 -0500

    implement acquisition of schemas from S3 objects

--- cloud-formation/python/s3_manager.py
+++ cloud-formation/python/s3_manager.py
@@ -1,41 +1,59 @@
 import boto3
 
 def get_S3bucket():
+    # This name is just temporary 
     return "tutorial-bucket-test1"
+    
+def get_substring(str, start, end):
+    return (str.split(start))[1].split(end)[0]
 
-def get_S3Services(bucket_name):
+def get_objects(bucket_name):
     """
-    Read and return the list of files in the Bucket
+    Read and return the content in the Bucket
     :param bucket_name
     :return: List of objects in the bucket
     """
     list_obj = []
     try:
-        # find list of files from S3 buckets 
+        # find list of files from S3 buckets
         s3 = boto3.resource('s3')
         bucket = s3.Bucket(bucket_name)
         for obj in bucket.objects.all():
             list_obj.append(obj.key)
-
-        return list(map(lambda key: key.split("_").pop(0) , list_obj))
-        #return list_obj
+        return list_obj
     
     except ClientError as e:
         logging.error(e)
         return False 
 
-def get_s3Model(bucket, service_name):
-    """
-    Build then call the specific object name for a given service
-    :param bucket
-    :param service_name
-    :return: content of the specific object in bucket
-    """
-    filename = service_name + "_schema.json"
+def get_schemas_paths(bucket_name):
+    apis, services = {}, {}
+    api_starts = 'api/'
+    service_starts = 'services/'
+    ends = '-schema.json'
 
-    return open_file_s3(bucket, filename)
+    objects = get_objects(bucket_name)
 
-def open_file_s3(bucket, filename):
+    for item in objects:
+        # identification des services
+        if item.endswith(ends):
+            if item.startswith(api_starts):
+                key = get_substring(item, api_starts, ends)
+                apis[key] = item
+            elif item.startswith('services'): 
+                key = get_substring(item, service_starts, ends)
+                services[key] = item
+            else:
+                raise Exception("unknown item in bucket: "+ item)
+
+    paths = {
+        "apis": apis,
+        "services": services
+    }
+        
+    return paths
+    
+def read_file(bucket, filename):
     """
     return the content of the object
     :param bucket
