2e6a9efdb095db21c305670646db2065bb66a92d
jolevesq <johann.levesque@canada.ca>
2023-02-28 11:59:25 -0500

    fix(ui): first ui draft

--- src/components/geolocator-content.tsx
+++ src/components/geolocator-content.tsx
@@ -26,9 +26,78 @@ export const GeolocatorPanelContent = (props: GeolocatorPanelContentProps): JSX.
   const { ui, react } = cgpv;
 
   const { useState, useEffect, useMemo } = react;
+  const { TextField, Select, Autocomplete, Button } = ui.elements;
 
+  const [query, setQuery] = useState('');
+  const [language, setLanguage] = useState(['en']);
+  const [services, setServices] = useState('');
+  const languages = [
+    ['en', 'English'],
+    ['fr', 'French'],
+  ];
+  const serviceKeys = [
+    ['nominatim', 'nominatim'],
+    ['geonames', 'geonames'],
+  ];
+
+  function callGeolocator() {
+    console.log(query);
+    console.log(language);
+    console.log(services);
+
+    getConvertedData();
+  }
+
+  async function getConvertedData(): Promise<any> {
+    const response = await fetch('https://fr59c5usw4.execute-api.ca-central-1.amazonaws.com/dev?q=meech');
+    const result: any = await response.json();
+    console.log(result)
+    return result;
+  };
+
+  function handleServices(event: Event, newValue: string[]) {
+    setServices(newValue.map((x) => x[1]).join(', '));
+  }
 
   return (
-    <div>This is the comming soon content!</div>
+    <>
+      <label htmlFor="filter">Search filter</label>
+      <TextField id="filter" type="text" onChange={(e: any) => setQuery(e.target.value)} />
+      <div style={{display: "grid", padding: "10px"}}>
+        <label htmlFor="language">Language filter (optional)</label>
+        <Select
+          id="language"
+          value={language}
+          onChange={(e: any) => setLanguage(e.target.value)}
+          inputLabel={{
+            id: 'select-variable',
+          }}
+          menuItems={languages.map(([value, label]) => ({
+            key: value,
+            item: {
+              value,
+              children: label,
+            },
+          }))}
+        />
+      </div>
+      <Autocomplete
+        style={{display: "grid", paddingBottom: "20px"}}
+        fullWidth
+        multiple={true}
+        disableCloseOnSelect
+        disableClearable={false}
+        id="service-key"
+        options={serviceKeys}
+        getOptionLabel={(option) => `${option[1]} (${option[0]})`}
+        renderOption={(props, option) => <span {...props}>{option[1]}</span>}
+        onChange={handleServices as any}
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        renderInput={(params) => <TextField {...params} label="Select Service keys" />}
+      />
+      <Button tooltip="Process Data" tooltipPlacement="right" type="text" variant="contained" onClick={() => callGeolocator()}>
+        Process Data
+      </Button>
+    </>
   );
 };
