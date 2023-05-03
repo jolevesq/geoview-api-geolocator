150c22d4fe2adbf2e40ba10fdba78546d6966711
Pascal Vautour <pascal.vautour@nrcan-rncan.gc.ca>
2023-05-03 16:32:07 -0400

    the api now deploys and attaches to the lambda.

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,2 +1,9 @@
+echo "bucket deploy starting."
 source ./scripts/bucket-deploy.sh
+echo "bucket deploy ending."
+echo "zip and deploy starting."
 source ./scripts/zip-and-deploy-lambda.sh
+echo "zip and deploy ending."
+echo "api deploy starting."
+source ./scripts/api.sh
+echo "api deploy ending."
--- deployment/scripts/zip-and-deploy-lambda.sh
+++ deployment/scripts/zip-and-deploy-lambda.sh
@@ -3,6 +3,6 @@ API_LAMBDA_KEY=build-artifacts/api-lambda-$(date '+%Y-%m-%d_%H:%M:%S').zip
 zip -r $API_LAMBDA_KEY api-lambda/
 aws s3 cp $API_LAMBDA_KEY s3://geolocator-dev-cf-2/$API_LAMBDA_KEY
 aws cloudformation deploy --template-file cloudformations/api-lambda.yml \
---stack-name pascal-geolocator-lambda \
+--stack-name pascalGeolocatorLambda \
 --capabilities CAPABILITY_NAMED_IAM \
 --parameter-overrides ApiLambdaKey=$API_LAMBDA_KEY
