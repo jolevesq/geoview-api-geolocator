75561d1c0d355e767baf925f61dc1aa6a2bb4a49
Pascal Vautour <pascal.vautour@nrcan-rncan.gc.ca>
2023-05-03 16:49:49 -0400

    the file structure in the lambda is now correct.

--- deployment/scripts/zip-and-deploy-lambda.sh
+++ deployment/scripts/zip-and-deploy-lambda.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 API_LAMBDA_KEY=build-artifacts/api-lambda-$(date '+%Y-%m-%d_%H:%M:%S').zip
-zip -r $API_LAMBDA_KEY api-lambda/
+cd api-lambda && zip -r ../$API_LAMBDA_KEY ./ && cd ../
 aws s3 cp $API_LAMBDA_KEY s3://geolocator-dev-cf-2/$API_LAMBDA_KEY
 aws cloudformation deploy --template-file cloudformations/api-lambda.yml \
 --stack-name pascalGeolocatorLambda \
