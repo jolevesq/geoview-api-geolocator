b1d579603d05f7802f296f0b395736e57a6a51bd
Pascal Vautour <pascal.vautour@nrcan-rncan.gc.ca>
2023-05-11 11:02:59 -0400

    refactor.

--- backend/scripts/zip-and-deploy-lambda.sh
+++ backend/scripts/zip-and-deploy-lambda.sh
@@ -2,9 +2,10 @@
 echo "zip and deploy starting."
 API_LAMBDA_KEY=build-artifacts/api-lambda-$(date '+%Y-%m-%d_%H:%M:%S').zip
 cd api-lambda && zip -r ../$API_LAMBDA_KEY ./ && cd ../
-aws s3 cp $API_LAMBDA_KEY s3://geolocator-dev-cf-2/$API_LAMBDA_KEY
+bucket_name=$(aws cloudformation describe-stacks --stack-name GeolocatorApiS3Bucket | jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="GeolocatorApiS3BucketName") | .OutputValue');
+aws s3 cp $API_LAMBDA_KEY s3://$bucket_name/$API_LAMBDA_KEY
 aws cloudformation deploy --template-file cloudformations/api-lambda.yml \
---stack-name pascalGeolocatorLambda \
+--stack-name GeolocatorApiLambda \
 --capabilities CAPABILITY_NAMED_IAM \
 --parameter-overrides ApiLambdaKey=$API_LAMBDA_KEY;
 echo "zip and deploy ending."
